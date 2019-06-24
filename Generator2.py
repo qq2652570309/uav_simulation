90daa5c56a6d8ee2a8138b6726d15a0772177fc9
wentian MAC laptop <wbai1295@gmail.com>
2019-06-24 16:50:30 -0400

    Generator

---
+++ Generator2.py
@@ -0,0 +1,8 @@
+import numpy as np
+from grid import Grid
+
+class Generator2:
+    def __init__(self, width=0, height=0, num = 0):  
+        self.height = height
+        self.width = width
+        self.num = num
