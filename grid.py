b1edee9fb8fbd1e0eb3602c112cc2c40c02d57be
Wentan Bai <wentan1995@gmail.com>
2019-06-25 13:23:43 -0400

    grid

--- grid.py
+++ grid.py
@@ -2,23 +2,27 @@ import numpy as np
 
 class Grid:
 
-    def __init__(self, x=0, y=0):
-        self.ROW = x
-        self.COLUMN = y
-        self.grid = np.zeros((x, y), dtype=np.int16)
+    def __init__(self, row=0, col=0):
+        self.ROW = row
+        self.COLUMN = col
+        self.grid = np.zeros((self.ROW, self.COLUMN), dtype=np.int16)
 
     def getGrid(self):
         return self.grid
 
+    
     def drawPath(self, startX, startY, endX, endY):
-        if endX > startX:
-            self.grid[startX:endX+1, endY] = 1
-        else:
-            self.grid[endX:startX+1, endY] = 1 
-
+        # first row then column
         if endY > startY:
-            self.grid[startX, startY:endY+1] = 1
+            self.grid[startX, startY:endY+1] += 1
         else:
-            self.grid[startX, endY:startY+1] = 1
+            self.grid[startX, endY:startY+1] += 1
+
+        if endX > startX:
+            self.grid[startX+1:endX+1, endY] += 1
+        else:
+            self.grid[endX:startX, endY] += 1 
+
+        
     
     
