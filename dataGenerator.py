3826f54e1dff9b2c9da7cd9846c99132c4611fbc
Wentan Bai <wentan1995@gmail.com>
2019-06-25 09:47:47 -0400

    dataset

--- dataGenerator.py
+++ dataGenerator.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class dataGenerator:
     def __init__(self, row=0, column=0):  
@@ -9,7 +10,9 @@ class dataGenerator:
     def genertating(self):
         for r in range(self.dataSet.shape[0]):
             for c in range(self.dataSet.shape[1]):
-                self.dataSet[r][c] = np.arange(self.row*self.column)
+                launching_rate = random.uniform(0, 1)
+                destination_rate = np.random.dirichlet(np.ones(self.row * self.column),size=1).flatten()
+                self.dataSet[r][c] = launching_rate * destination_rate
 
     def getDataSet(self):
         return self.dataSet
--- protype.py
+++ protype.py
@@ -3,22 +3,26 @@ from grid import Grid
 
 
 # create a matrix, sum of its elements is 1 
-row = 5
+row = 2
 col = 4
 
-s = np.random.dirichlet(np.ones(col),size=row)
+s = np.random.dirichlet(np.ones(row * col),size=1).flatten()
+print(s)
+launching_rate = 2
+res = launching_rate * s
+print(res)
 
 ############################################################
 
-row = 3
-column = 3
+# row = 3
+# column = 3
 
-test = Grid(x=row, y=column)
-test.drawPath(startX=0, startY=2, endX=0, endY=2)
-# print(test.getGrid())
+# test = Grid(x=row, y=column)
+# test.drawPath(startX=0, startY=2, endX=0, endY=2)
+# # print(test.getGrid())
 
-arr = np.arange(row*column)
-np.random.shuffle(arr)
+# arr = np.arange(row*column)
+# np.random.shuffle(arr)
 
-for n in arr:
-    print("{0} in array is: ({1}, {2})".format(n, n//row, n%column))
+# for n in arr:
+#     print("{0} in array is: ({1}, {2})".format(n, n//row, n%column))
