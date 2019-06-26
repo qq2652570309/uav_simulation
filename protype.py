0f96af55541b359aa80986eaea174d246f115a89
Wentan Bai <wentan1995@gmail.com>
2019-06-26 16:46:30 -0400

    grid

--- protype.py
+++ protype.py
@@ -1,10 +1,6 @@
 import numpy as np
-from grid import Grid
 
 
-# create a matrix, sum of its elements is 1 
-row = 2
-col = 4
 
 s = np.random.dirichlet(np.ones(row * col),size=1).flatten()
 print(s)
