853dec93d88c062a5941b3c56d62a38b038ad5b8
wentian MAC laptop <wbai1295@gmail.com>
2019-06-26 09:32:17 -0400

    unmerege

--- protype.py
+++ protype.py
@@ -11,18 +11,3 @@ print(s)
 launching_rate = 2
 res = launching_rate * s
 print(res)
-
-############################################################
-
-# row = 3
-# column = 3
-
-# test = Grid(x=row, y=column)
-# test.drawPath(startX=0, startY=2, endX=0, endY=2)
-# # print(test.getGrid())
-
-# arr = np.arange(row*column)
-# np.random.shuffle(arr)
-
-# for n in arr:
-#     print("{0} in array is: ({1}, {2})".format(n, n//row, n%column))
