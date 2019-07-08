444babcc44621ee386879304ef039c36fb7233e3
wentian <wbai1295@gmail.com>
2019-07-07 23:45:54 -0400

    v1

--- mytrain.py
+++ mytrain.py
@@ -10,8 +10,11 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 uav_data = np.load("trainingSets.npy")
+uav_data = np.transpose(uav_data,(0,2,3,1))
 
 uav_label = np.load("groundTruths.npy")
+uav_label = np.transpose(uav_label,(0,2,3,1))
+
 uav_label = (uav_label - np.min(uav_label)) / np.max(uav_label) - np.min(uav_label)
 print(uav_label.shape)
 print(np.min(uav_label))
@@ -24,23 +27,23 @@ print(np.median(uav_label))
 (x_test, y_test) = uav_data[850:], uav_label[850:]
 # y_test = y_test.reshape((150, 16, 16, 1))
 
-input_img = Input(shape=(32, 16, 16))
+input_img = Input(shape=(16, 16, 32))
 
-x = Conv2D(16, (3, 3), activation='relu', padding='same', data_format='channels_first')(input_img)
-x = MaxPooling2D((2, 2), padding='same', data_format='channels_first')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same', data_format='channels_first')(x)
-x = MaxPooling2D((2, 2), padding='same', data_format='channels_first')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same', data_format='channels_first')(x)
-encoded = MaxPooling2D((2, 2), padding='same', data_format='channels_first')(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
 # print('after 3rd MaxP: ', encoded.shape)
 
-x = Conv2D(8, (3, 3), activation='relu', padding='same', data_format='channels_first')(encoded)
-x = UpSampling2D((2, 2), data_format='channels_first')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same', data_format='channels_first')(x)
-x = UpSampling2D((2, 2), data_format='channels_first')(x)
-x = Conv2D(16, (3, 3), activation='relu', padding='same', data_format='channels_first')(x)
-x = UpSampling2D((2, 2), data_format='channels_first')(x)
-decoded = Conv2D(32, (3, 3), activation='sigmoid', padding='same', data_format='channels_first')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(32, (3, 3), activation='sigmoid', padding='same')(x)
 # print(decoded.shape)
 
 autoencoder = Model(input_img, decoded)
