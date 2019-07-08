21a185d2f10c9d132a8d71049d4a449096522f69
wentian <wbai1295@gmail.com>
2019-07-07 23:48:22 -0400

    main

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(filename='log.txt', format='%(levelname)s:%(message)s', leve
 
 logging.info('Started')
 # s = Simulator(iteration=2, row=4, column=4, time=5, startPointsNum=3, endPointsNum=3)
-s = Simulator(iteration=1000, row=16, column=16, time=60, startPointsNum=30, endPointsNum=30)
+s = Simulator(iteration=1000, row=16, column=16, time=32, startPointsNum=30, endPointsNum=30)
 # s = Simulator(iteration=100, row=64, column=64, time=60, startPointsNum=120, endPointsNum=120)
 s.generate()
 logging.info('Finished')
